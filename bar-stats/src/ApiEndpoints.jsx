0cb5c00c73d6d4c453e95414f585df4cfa6b226f
Kevin <kevin.dicola@gmail.com>
2024-02-13 23:03:30 +0900

    add endpoints and mock data

---
+++ bar-stats/src/ApiEndpoints.jsx
@@ -0,0 +1,6 @@
+export const GetReplay = async (replayId) => {
+  return await fetch(`https://api.bar-rts.com/replays/${replayId}`);
+}
+export const GetForPlayer = async (playerId) => {
+  return await fetch(`https://api.bar-rts.com/replays?page=1&limit=100000&hasBots=false&endedNormally=true&preset=team&players=${playerId}`);
+}
