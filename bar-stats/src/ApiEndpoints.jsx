6f42c7462f5ebd2f4201ffdfa8e191fb932bab8b
Kevin <kevin.dicola@gmail.com>
2024-02-15 23:10:22 +0900

    query replays for player

--- bar-stats/src/ApiEndpoints.jsx
+++ bar-stats/src/ApiEndpoints.jsx
@@ -1,6 +1,8 @@
 export const GetReplay = async (replayId) => {
-  return await fetch(`https://api.bar-rts.com/replays/${replayId}`);
-}
+  return await fetch(`https://api.bar-rts.com/replays/${replayId}`, { cache: "force-cache" });
+};
 export const GetForPlayer = async (playerId) => {
-  return await fetch(`https://api.bar-rts.com/replays?page=1&limit=100000&hasBots=false&endedNormally=true&preset=team&players=${playerId}`);
-}
+  return await fetch(
+    `https://api.bar-rts.com/replays?page=1&limit=100000&hasBots=false&endedNormally=true&preset=team&players=${playerId}`
+  );
+};
--- bar-stats/src/helper/usePlayerData.jsx
+++ bar-stats/src/helper/usePlayerData.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { findWinLoss } from "../ExtractStats";
 import { mockDataForPlayer } from "../mockData.js";
+import { GetReplay } from "../ApiEndpoints";
+
+const BATCHSIZE = 5;
 
 export const usePlayerData = (targetPlayerId) => {
   const [processedData, setProcessedData] = useState({ hasData: false, isLoading: false });
@@ -16,6 +19,8 @@ export const usePlayerData = (targetPlayerId) => {
       // This will be a query
       const receivedData = mockDataForPlayer.data;
 
+      const allReplayIds = receivedData.map((game) => game.id);
+      const allReplayData = await queryAllReplays(allReplayIds, null);
       // Process data
       const processedData = {};
       processedData.winStats = findWinLoss(receivedData, playerId);
@@ -29,4 +34,23 @@ export const usePlayerData = (targetPlayerId) => {
   }, [targetPlayerId]);
 
   return processedData;
+};
+
+const queryAllReplays = async (allReplayIds, outputObj) => {
+  if (outputObj == null || outputObj == undefined) outputObj = {};
+  for (let i = 0; i < allReplayIds.length; i += BATCHSIZE) {
+    const endIndex = Math.min(i + BATCHSIZE, allReplayIds.length - 1);
+    console.log(`querying replays ${i} to ${endIndex}`);
+    const promises = [];
+    let x = i;
+    while (x <= endIndex) {
+      promises.push(GetReplay(allReplayIds[x]));
+      x += 1;
+    }
+    const responses = await Promise.all(promises);
+    const jsonPromises = responses.map((response) => response.json());
+    const details = await Promise.all(jsonPromises);
+    details.forEach((detail) => (outputObj[detail.id] = detail));
+  }
+  return outputObj;
 };
