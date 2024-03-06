9ae73087ae6bfb5b9da490a72e3f8283fb5278fd
Kevin <kevin.dicola@gmail.com>
2024-03-06 18:17:16 +0900

    fix for win ratio percentage formatting very long

--- bar-stats/src/components/WinLossStats.jsx
+++ bar-stats/src/components/WinLossStats.jsx
@@ -1,28 +1,27 @@
 import React from "react";
 
 export const WinLossStats = (props) => {
-  const {data} = props;
-  if(data.hasData === false)
-    return (<p> no data :c </p>);
+  const { data } = props;
+  if (data.hasData === false) return <p> no data :c </p>;
 
   return (
-     <div className="stats shadow w-full h-full text-3xl ">
-        <div className="stat">
-          <div className="stat-title">Wins</div>
-          <div className="stat-value text-primary text-8xl">{data.winStats.wins}</div>
-        </div>
-        <div className="stat">
-          <div className="stat-title">Losses</div>
-          <div className="stat-value text-secondary text-7xl">{data.winStats.losses}</div>
-        </div>
-        <div className="stat">
-          <div className="stat-title">Games</div>
-          <div className="stat-value text-7xl">{data.winStats.games}</div>
-        </div>
-        <div className="stat">
-          <div className="stat-title">Ratio</div>
-          <div className="stat-value text-7xl">{data.winStats.winRatio.toFixed(2)*100 + "%"}</div>
-        </div>
+    <div className="stats shadow w-full h-full text-3xl ">
+      <div className="stat">
+        <div className="stat-title">Wins</div>
+        <div className="stat-value text-primary text-8xl">{data.winStats.wins}</div>
       </div>
-  )
-}
+      <div className="stat">
+        <div className="stat-title">Losses</div>
+        <div className="stat-value text-secondary text-7xl">{data.winStats.losses}</div>
+      </div>
+      <div className="stat">
+        <div className="stat-title">Games</div>
+        <div className="stat-value text-7xl">{data.winStats.games}</div>
+      </div>
+      <div className="stat">
+        <div className="stat-title">Ratio</div>
+        <div className="stat-value text-7xl">{(data.winStats.winRatio * 100).toFixed(2) + "%"}</div>
+      </div>
+    </div>
+  );
+};
