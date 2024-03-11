55266283dda5c86480091876eb531aaacfc66a5f
Kevin <kevin.dicola@gmail.com>
2024-03-11 21:44:04 +0900

    map density plot

---
+++ bar-stats/src/components/MapStartDensityPlot.jsx
@@ -0,0 +1,51 @@
+import * as Plot from "@observablehq/plot";
+import { useEffect, useRef } from "react";
+
+export const MapStartDensityPlot = (props) => {
+  const { data } = props;
+  const containerRef = useRef();
+
+  useEffect(() => {
+    //this might not need to be an effect
+    if (data === undefined) return;
+
+    const mapWidth = data.mapSize.width * 512;
+    const mapHeight = data.mapSize.height * 512;
+
+    const filename = `url(https://api.bar-rts.com/maps/${data.filename}/texture-thumb.jpg)`;
+
+    const plot = Plot.plot({
+      inset: 0,
+      margin: 0,
+      width: 420,
+      height: 420,
+      style: {
+        background: filename,
+        backgroundSize: "cover",
+      },
+      marks: [
+        //Plot.frame(),
+        Plot.density(data.startPositions, {
+          x: "x",
+          y: "z",
+          weight: () => 1,
+          fill: "density",
+          fillOpacity: 0.4,
+          thresholds: 10,
+        }),
+        Plot.dot(data.startPositions, { x: "x", y: "z" }),
+        //Plot.dot(corners, { x: "x", y: "z", fill: "red" }),
+      ],
+      x: {
+        domain: [mapWidth, 0],
+      },
+      y: {
+        domain: [0, mapHeight],
+      },
+    });
+    containerRef.current.append(plot);
+    return () => plot.remove();
+  }, [data]);
+
+  return <div className="" ref={containerRef} />;
+};
--- bar-stats/src/components/MapStatsOverview.jsx
+++ bar-stats/src/components/MapStatsOverview.jsx
@@ -4,6 +4,7 @@ import { THEMECOLORS } from "../helper/colors";
 import { bin } from "d3-array";
 import { scaleUtc } from "d3-scale";
 import { BarChart, XAxis, YAxis, Tooltip, Bar, ResponsiveContainer } from "recharts";
+import { MapStartDensityPlot } from "./MapStartDensityPlot";
 
 export const MapStatsOverview = (props) => {
   const { data } = props;
@@ -15,19 +16,6 @@ export const MapStatsOverview = (props) => {
     };
   };
 
-  /*const getMapDateBins = (map) => {
-    //const binner = bin().thresholds(5);
-    //const bins = binner(map.startTimes);
-    const dates = {};
-    map.startTimes.forEach((time) => {
-      const dateString = time.toLocaleDateString("en-GB");
-      if (!(dateString in dates)) dates[dateString] = 0;
-      dates[dateString] += 1;
-    });
-    const output = Object.keys(dates).map((k) => ({ date: k, count: dates[k] }));
-    return output;
-  };
-  */
   const getMapDurationBins = (map) => {
     const binner = bin().thresholds(thresholdTime(10));
     const bins = binner(map.durations);
@@ -63,8 +51,8 @@ export const MapStatsOverview = (props) => {
           {map.cleanName} - {map.wins} wins, {map.count} games ({((map.wins / map.count) * 100).toFixed(0)}%)
         </div>
         <div className="collapse-content w-full prose max-w-none">
-          <div className="flex w-full">
-            <div className="flex-grow basis-1/3">
+          <div className="flex w-full gap-8">
+            <div className="flex-grow basis-1/5">
               <h4 className="mt-2">Average game length: </h4>
               <span>{msToString(durStat.mean)}</span>
               <h4>Median game length: </h4>
@@ -78,7 +66,7 @@ export const MapStatsOverview = (props) => {
               <h4>Game length upper quartile: </h4>
               <p>{msToString(durStat.upperQuartile)}</p>
             </div>
-            <div className="flex-grow basis-2/3">
+            <div className="flex-grow basis-1/3">
               <div className="flex flex-col h-full">
                 <div className="text-center ">
                   <h3 className="mt-0">Game Durations</h3>
@@ -94,6 +82,9 @@ export const MapStatsOverview = (props) => {
                   </ResponsiveContainer>
                 </div>
               </div>
+            </div>
+            <div className="flex-grow basis-1/3">
+              <MapStartDensityPlot data={map} />
             </div>
           </div>
         </div>
